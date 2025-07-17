bac3a3f5ea1e7d8480da256631663eebce74a886
shakil-ahmed-billal <itzxhakil@gmail.com>
2025-07-17 21:04:08 +0600

    component redesign done

--- src/components/videos/Player.jsx
+++ src/components/videos/Player.jsx
@@ -1,33 +1,20 @@
-import React from 'react'
 
-const getDriveVideoUrl = (url) => {
-  // Match Google Drive preview URL
-  const match = url.match(/drive\.google\.com\/file\/d\/([\w-]+)\/preview/)
-  if (match) {
-    const fileId = match[1]
-    // Construct direct video stream URL
-    return `https://drive.google.com/uc?export=preview&id=${fileId}`
-  }
-  return null
-}
 
-const Player = ({ videoUrl }) => {
-  const driveVideo = getDriveVideoUrl(videoUrl)
+const Player = ({ videoFile }) => {
+
+  console.log("Video Info:", videoFile)
+
   return (
     <div>
       <div className="aspect-video">
-        {!videoUrl ? (
-          <video src={driveVideo} controls className="w-full h-full" />
-        ) : (
-          <iframe
-            src={videoUrl}
-            title="Video player"
-            frameBorder="0"
-            allow="autoplay; fullscreen; accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            className="w-full h-full"
-          ></iframe>
-        )}
+        <iframe
+          src={videoFile?.link}
+          title="Video player"
+          frameBorder="0"
+          allow="autoplay; fullscreen; accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          className="w-full h-full"
+        ></iframe>
       </div>
     </div>
   )
